a2985083917092568d427c8c1d59bcb3b5977081
Joanna Gorska <gorska919@gmail.com>
2021-11-04 14:46:56 +0000

    Add test to see if game object contains scores

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,24 @@
 /**
  * @jest-environment jsdom
  */
+
+// import functions...
+
+// this loads DOM before each test is run
+
+beforeAll(() => {
+    // installes fs library
+    let fs = requrie("fs");
+    let fileContents = fs.readFileSync("../index.html", "utf-8");
+
+    // writes the content of index.html into fileContents
+    document.write(fileContents);
+    document.close();
+
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+}) 
