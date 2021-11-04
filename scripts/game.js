177979012c88feecb4dd74816738d2def7e0760a
Joanna Gorska <gorska919@gmail.com>
2021-11-04 18:24:42 +0000

    Add ids of the buttons to the choices array, tests passed

--- scripts/game.js
+++ scripts/game.js
@@ -3,7 +3,7 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: [],
+    choices: ["button1", "button2", "button3", "button4"],
 }
 
 // exports the object, curly braces are needed because more than will be exported
