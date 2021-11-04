182c132d4b1500bf0b31cf9b54bbd866848086bb
Joanna Gorska <gorska919@gmail.com>
2021-11-04 15:04:29 +0000

    Fix spelling errors and errors in path

---
+++ scripts/game.js
@@ -0,0 +1,8 @@
+
+let game = {
+    score: 0,
+}
+
+// exports the object, curly braces are needed because more than will be exported
+
+module.exports = {game};
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,14 +2,18 @@
  * @jest-environment jsdom
  */
 
+
+
 // import functions...
+
+const { game } = require("../game")
 
 // this loads DOM before each test is run
 
 beforeAll(() => {
     // installes fs library
-    let fs = requrie("fs");
-    let fileContents = fs.readFileSync("../index.html", "utf-8");
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
 
     // writes the content of index.html into fileContents
     document.write(fileContents);
