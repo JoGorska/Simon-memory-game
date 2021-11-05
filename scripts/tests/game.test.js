b27577d3620c29aea01d8665190dd5330c031e51
Joanna Gorska <gorska919@gmail.com>
2021-11-05 11:10:38 +0000

    Add test to see if newGame function clears the score to 0 that is displayed for the user in the DOM

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -41,19 +41,28 @@ describe("game object contains correct keys", () => {
 
 describe("newGame function clears the values in the keys of the game object", () => {
     beforeAll(() => {
+
+        // sets the keys in the game object to some random values, to test if game function will clear this.
         game.score = 42;
         game.playerMoves = ["button1"];
         game.currentGame = ["button2"];
+
+        // sets the score, that is displayed to the user to 42 to see if newGame function will clear it
+        document.getElementById("score").innerText = "42"
         newGame();
+
     });
     test("expect score to be 0", () => {
         
-        expect(game.score).toEqual(0)
+        expect(game.score).toEqual(0);
     });
     test("expect playerMoves to be empty array", () => {
-        expect(game.playerMoves.length).toEqual(0)
+        expect(game.playerMoves.length).toEqual(0);
     });
     test("expect currentGame to be empty array", () => {
-        expect(game.currentGame.length).toEqual(0)
+        expect(game.currentGame.length).toEqual(0);
     });
+    test("should display 0 for the element with id score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
+    })
 });
